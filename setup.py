489c144d91d22f721cc2fd6da11974fb791b37d3
lihao <lih@htcxfund.com>
2025-03-28 09:48:22 +0800

    v0.0.10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_requirements():
 
 setup(
     name="schd",
-    version="0.0.9",
+    version="0.0.10",
     url="https://github.com/kevenli/schd",
     packages=find_packages(exclude=('tests', 'tests.*')),
     install_requires=read_requirements(),
