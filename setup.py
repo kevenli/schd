ccdea454cb073bd291986d7cef2ea75194629d5d
kevenli <pbleester@gmail.com>
2025-06-28 08:07:21 +0000

    v0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_requirements():
 
 setup(
     name="schd",
-    version="0.0.16",
+    version="0.1.0",
     url="https://github.com/kevenli/schd",
     packages=find_packages(exclude=('tests', 'tests.*')),
     install_requires=['apscheduler<4.0', 'pyaml', 'aiohttp'],
